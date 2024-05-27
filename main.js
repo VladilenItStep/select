9df4a9d48116156c3a69316b2d1108e7c6ae369c
Vlad Step <sobol_v92@mail.ru>
2024-05-27 20:46:06 +0300

    the end

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const options = [{
         label: 'Витебск',
-        value: 2
+        value: 2,
+        group: 'Витебская область'
     },
     {
         label: 'Могилев',
-        value: 6
+        value: 6,
+        group: 'Первая'
     },
     {
         label: 'Гомель',
-        value: 3
+        value: 3,
+        group: 'Вторая'
+
     },
     {
         label: 'Брест',
-        value: 1
+        value: 1,
+        group: 'Вторая'
+
     },
     {
         label: 'Гродно',
-        value: 4
+        value: 4,
+        group: 'Третья'
+
     },
     {
         label: 'Минск',
@@ -25,6 +33,12 @@ const options = [{
     {
         label: 'Москва',
         value: 777
+    },
+    {
+        label: 'Орша',
+        value: 123,
+        group: 'Витебская область'
+
     },
 ]
 
@@ -35,25 +49,45 @@ class Select {
         this.$select.style.setProperty('--selected-width', width);
         this.$label = document.querySelector('.select__label');
         this.$dropDown = document.querySelector('.select__drop-down');
-        this.itemsForDropDown = this.options.map(({ label, value }) => {
-            return `<li data-id=${value}>${label}</li>`
-        }).join('')
 
+        // this.itemsForDropDown = this.options.map(({ label, value }) => {
+        //     return `<li data-id=${value}>${label}</li>`
+        // }).join('')
+
+        this.itemsForDropDown = this.initGroup(this.options).map(([key, items]) => {
+            if (key) {
+
+                const groupList = items.map(({ label, value }) => {
+                    return `<li data-id=${value}>${label}</li>`
+                }).join('')
+
+                return `<ul style="padding: 8px"><span style="color: gray">${key}</span>${groupList}</ul>`
+
+            } else {
+                return items.map(({ label, value }) => {
+                    return `<li data-id=${value}>${label}</li>`
+                }).join('')
+            }
+        }).join('')
 
         this.$dropDown.insertAdjacentHTML('afterbegin', this.itemsForDropDown)
 
-
         this.$select.addEventListener('click', (e) => {
-            console.log(e.target);
-            console.log(e.target.tagName);
-            console.log(e.target.classList);
-
             if (e.target.classList.contains('select__label')) {
-                console.log('ты кликнул на лебел');
+
+
+                // if (this.$select.classList.contains('active')) {
+                //     this.close()
+                // } else {
+                //     this.open()
+                // }
+
+                this.$select.classList.toggle('active')
             } else {
                 if (e.target.tagName.toLowerCase() === 'li') {
                     console.log('ты кликнул на ли');
                     this.selectItem(+e.target.dataset.id)
+                    this.close()
                 }
             }
         })
@@ -65,10 +99,41 @@ class Select {
         this.$label.innerHTML = selectedElement.label
     }
 
+    open() {
+        this.$select.classList.add('active')
+    }
+
+    close() {
+        this.$select.classList.remove('active')
+    }
+
+    initGroup(items) {
+        const group = new Map()
+        console.log('group', group);
+
+        items.forEach((item) => {
+
+            if (!group.has(item.group)) {
+                group.set(item.group, [item])
+            } else {
+                group.set(item.group, [...group.get(item.group), item])
+            }
+        })
+        return Array.from(group.entries())
+    }
+
 
 }
 
 
 const customSelect = new Select('.select', options, '350px')
 
-console.log(customSelect);
+
+// [
+//     ['Первая', [{ label: 'Витебск' }, { label: 'Могилев' }]],
+//     ['Вторая', [{ label: 'Гомель' }, { label: 'Брест' }]],
+//     ['Третья', [{ label: 'Гродно' }, ]]
+//     [undefined, [
+//         [{ label: 'Минск' }, { label: 'Москва' }]
+//     ]]
+// ]
